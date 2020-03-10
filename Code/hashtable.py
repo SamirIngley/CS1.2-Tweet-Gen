5aa06a2e8238aa5212be4baad41ccea961640674
SamirIngley <samir.ingle7@gmail.com>
2020-03-09 17:45:32 -0700

    beginning nth order markov

--- Code/hashtable.py
+++ Code/hashtable.py
@@ -1,4 +1,3 @@
-#!python
 
 from linkedlist import LinkedList, Node
 
--- Code/markov_chain.py
+++ Code/markov_chain.py
@@ -2,50 +2,50 @@ import random
 from clean_text import clean
 from dictogram import Dictogram
 
+''' General vocab:
+Stack : Last in First Out
+Queue : First in First out
+For a markov chain of n length we want to use a queue. Where we store the length n words and 
+add the next word while removing the last word to traverse the corpus and save the states. 
+'''
+
 class Markov():
-    def __init__(self, corpus):
+    def __init__(self, size, corpus):
         self.corpus = clean(corpus)
         self.states = {}
         self.chain()
+        self.size = size
     
     def chain(self):
 
-        last_word = None
+        # last_word = None
  
+        # for word in self.corpus:
+        #     if last_word is not None: # set last word line 14
+
+        #         if last_word not in self.states: # if we haven't seen this word before
+        #             self.states[last_word] = Dictogram() # empty dictogram as value
+        #         self.states[last_word].add_count(word) # add word to last word histogram
+
+        #     last_word = word # set word as last_word
+
+
+        # build the chain with states
+        queue = []
+        prev_q = []
+
         for word in self.corpus:
-            if last_word is not None: # set last word line 14
-
-                if last_word not in self.states: # if we haven't seen this word before
-                    self.states[last_word] = Dictogram() # empty dictogram as value
-                self.states[last_word].add_count(word) # add word to last word histogram
-
-            last_word = word # set word as last_word
-
-
-        for i,k in zip(self.corpus[0::2], self.corpus[1::2]):  # in the [ ] start at the first value, skip ahead by the last value
-            print(i, k)
+        
+            while len(queue) < self.size:
+                queue.append(word)
             
-        for i,k in zip(self.corpus, self.corpus[1:]):  # in the [ ] start at the first value, skip ahead by the last value
-            print("SECOND VERSION", i, k)
+            if prev_q is not None:
+                if prev_q not in self.states:
+                    self.states[prev_q] = Dictogram()
+                self.states[prev_q].add_count(queue)
 
-        # current = word
-        # if prev != None:
-
-        #     instance = (prev, current)
-
-        #     if instance not in self.states:
-        #         self.states[instance] = Dictogram()
-
-        #     if len(self.states) == 0:
-        #         continue
-
-
-                
-
-        #     self.states[instance].add_count()
-
-
-        # prev = word 
+            prev_q = queue
+            queue.pop(0)
 
              
     def __str__(self):
@@ -76,7 +76,7 @@ class Markov():
 
 if __name__ == '__main__':
     source = 'one fish two fish red fish blue fish'
-    markov = Markov('source.txt')
+    markov = Markov(2,'source.txt')
     print(markov.states)
     # print('')
     # print(markov.chain())
